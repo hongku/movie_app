3444892329f6a04ce9e93eedb459cffd581963f7
hongku <hongsun3152@gmail.com>
2019-09-22 14:45:50 +0900

    Updates

---
+++ precache-manifest.77193fbe728cd2b730906165157aaa48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1304f6bec74116144b7c7ca099e2f7f4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0cbca162932d1227585b",
+    "url": "/movie_app/static/css/main.7d9b373d.chunk.css"
+  },
+  {
+    "revision": "dee1dfeb0ee4e5d3fc91",
+    "url": "/movie_app/static/js/2.a9a352c0.chunk.js"
+  },
+  {
+    "revision": "0cbca162932d1227585b",
+    "url": "/movie_app/static/js/main.e694bfba.chunk.js"
+  },
+  {
+    "revision": "0a5c0ad77459cf3396ae",
+    "url": "/movie_app/static/js/runtime~main.ebbd9dda.js"
+  }
+]);
